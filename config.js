994e26da2d8f9111d395e789c634cc5e0ec357e8
MrJOKER20 <139184454+MrJOKER20@users.noreply.github.com>
2025-01-10 15:46:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ function convertToBool(text, fault = 'true') {
 //--------------------------Kang Jinhuyk---------------------------------------
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "yQpikLJb#g8GN9MeZpX2GesdZgdVgwbke1QKaDqEKfy6yr3ukNu4",
+SESSION_ID: process.env.SESSION_ID || "yQpikLJb#zN4VnYiL#Sb_5tBseoi-iJxMXKmzeTLbiG7djr_jeaFE4-tCV8Og",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", //TRUE OR FALSE
 OWNER_NUMBER: process.env.OWNER_NUMBER || "237620746697",
 MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
@@ -39,7 +39,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".", // .,!/?
 FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "true",
 BOT_NAME: process.env.BOT_NAME || "『 ＪＩＮＨＵＹＫ-ＭＤ-Ｖ２ 』",
